75a0e45c60123818d92ff9a4932f5449c06604af
ferventdesert <buptzym@qq.com>
2016-05-05 20:50:21 +0800

    update,晚上自己做饭吃

--- 安居客/安居客.py
+++ 安居客/安居客.py
@@ -7,18 +7,27 @@ def get(ar,index):
 	else:
 		return ar[index];
 		
-def execute(ar,filter,func):
+def find(ar,filter):
 	for r in ar:
 		if filter(r):
-			func(r);
+			return r;
+	return None;
 unabled=[户型图存储方案,户型图存储,安居客户型列表,安居客评价,安居客楼盘详情,相册存储方案,安居客相册];
 for e in unabled:
 	e.etls[0].Enabled=False
 
-execute(详细参数.etls,lambda x:x.TypeName=='WriteFileTextTF',lambda x:x.Enabled==False);
+跳过控制=find(安居客核心流程.etls,lambda x:x.TypeName=='数量范围选择')
 
 #下面是可能需要修改的配置:
 ###################################################
+
+
+##要跳过的数量
+跳过控制.Skip=10
+
+
+#是否要将完整的Json信息保存到数据库中
+get(安居客核心流程.etls,-2).Enabled=False
 
 #是否要保存相册？不论是否保存，都会将相册的路径存入数据库中
 get(安居客相册.etls,-1).Enabled=True
@@ -27,7 +36,7 @@ get(安居客相册.etls,-1).Enabled=True
 get(户型图存储.etls,-1).Enabled=True	
 	
 #要采集的城市，使用正则表达式，如果包含全部城市，则写为''
-get(安居客城市.etls,-1).Script='上海'
+get(安居客城市.etls,-1).Script='南京'
 #户型图的存储路径
 get(户型图存储方案.etls,-2).Format='E:\安居客图片\{0}\户型图\{1}_{2}_{3}.jpg'
 #相册的存储路径
